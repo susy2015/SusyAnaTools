5a4e1a847c70412ce0ca3e748db322d395fb7c65
Nathaniel <pastika@cern.ch>
2015-06-12 17:45:36 -0500

    Baseline updates for zinv

--- Tools/baselineDef.h
+++ Tools/baselineDef.h
@@ -22,20 +22,26 @@ public:
     {
         bool debug = false;
         bool doIsoTrksVeto = false;
+        bool doMuonVeto = true;
 
         bool passBaseline = true;
         bool passBaselineNoTag = true;
 
         std::string jetVecLabel = "jetsLVec";
         std::string CSVVecLabel = "recoJetsBtag_0";
+        std::string METLabel    = "met";
+        std::string METPhiLabel = "metphi";
         if(spec.compare("Zinv") == 0) 
         {
             jetVecLabel = "jetsLVec";//"prodJetsNoMu_jetsLVec";
             CSVVecLabel = "recoJetsBtag_0";
+            METLabel    = "cleanMetPt";
+            METPhiLabel = "cleanMetPhi";
+            doMuonVeto  = false;
         }
 
         // Form TLorentzVector of MET
-        TLorentzVector metLVec; metLVec.SetPtEtaPhiM(tr.getVar<double>("met"), 0, tr.getVar<double>("metphi"), 0);
+        TLorentzVector metLVec; metLVec.SetPtEtaPhiM(tr.getVar<double>(METLabel), 0, tr.getVar<double>(METPhiLabel), 0);
 
         // Calculate number of leptons
         int nMuons = AnaFunctions::countMuons(tr.getVec<TLorentzVector>("muonsLVec"), tr.getVec<double>("muonsMiniIso"), tr.getVec<double>("muonsMtw"), AnaConsts::muonsMiniIsoArr);
@@ -43,7 +49,7 @@ public:
         int nIsoTrks = AnaFunctions::countIsoTrks(tr.getVec<TLorentzVector>("loose_isoTrksLVec"), tr.getVec<double>("loose_isoTrks_iso"), tr.getVec<double>("loose_isoTrks_mtw"), AnaConsts::isoTrksArr);
 
         // Calculate number of jets and b-tagged jets
-        int cntCSVS = AnaFunctions::countCSVS(tr.getVec<TLorentzVector>(jetVecLabel), tr.getVec<double>("recoJetsBtag_0"), AnaConsts::cutCSVS, AnaConsts::bTagArr);
+        int cntCSVS = AnaFunctions::countCSVS(tr.getVec<TLorentzVector>(jetVecLabel), tr.getVec<double>(CSVVecLabel), AnaConsts::cutCSVS, AnaConsts::bTagArr);
         int cntNJetsPt50Eta24 = AnaFunctions::countJets(tr.getVec<TLorentzVector>(jetVecLabel), AnaConsts::pt50Eta24Arr);
         int cntNJetsPt30Eta24 = AnaFunctions::countJets(tr.getVec<TLorentzVector>(jetVecLabel), AnaConsts::pt30Eta24Arr);
         int cntNJetsPt30      = AnaFunctions::countJets(tr.getVec<TLorentzVector>(jetVecLabel), AnaConsts::pt30Arr);
@@ -54,19 +60,12 @@ public:
 
         // Prepare jets and b-tag working points for top tagger
         std::vector<TLorentzVector> *jetsLVec_forTagger = new std::vector<TLorentzVector>(); std::vector<double> *recoJetsBtag_forTagger = new std::vector<double>();
-        AnaFunctions::prepareJetsForTagger(tr.getVec<TLorentzVector>(jetVecLabel), tr.getVec<double>("recoJetsBtag_0"), (*jetsLVec_forTagger), (*recoJetsBtag_forTagger));
+        AnaFunctions::prepareJetsForTagger(tr.getVec<TLorentzVector>(jetVecLabel), tr.getVec<double>(CSVVecLabel), (*jetsLVec_forTagger), (*recoJetsBtag_forTagger));
         if( debug ) std::cout<<"\njetsLVec_forTagger->size : "<<jetsLVec_forTagger->size()<<"  recoJetsBtag_forTagger->size : "<<recoJetsBtag_forTagger->size()<<"  passBaseline : "<<passBaseline<<std::endl;
 
         // Pass lepton veto?
         bool passLeptVeto = true, passMuonVeto = true, passEleVeto = true, passIsoTrkVeto = true;
-        if(spec.compare("Zinv") == 0)
-        {
-            // no requirement from baseline in Zinv case 
-        }
-        else
-        {
-            if( nMuons != AnaConsts::nMuonsSel ){ passBaseline = false; passBaselineNoTag = false; passLeptVeto = false; passMuonVeto = false; }
-        }
+        if( doMuonVeto && nMuons != AnaConsts::nMuonsSel ){ passBaseline = false; passBaselineNoTag = false; passLeptVeto = false; passMuonVeto = false; }
         if( nElectrons != AnaConsts::nElectronsSel ){ passBaseline = false; passBaselineNoTag = false; passLeptVeto = false; passEleVeto = false; }
         // Isolated track veto is disabled for now
         if( doIsoTrksVeto && nIsoTrks != AnaConsts::nIsoTrksSel ){ passBaseline = false; passBaselineNoTag = false; passLeptVeto = false; passIsoTrkVeto = false; }
@@ -90,7 +89,7 @@ public:
 
         // Pass the baseline MET requirement?
         bool passMET = true;
-        if( tr.getVar<double>("met") < AnaConsts::defaultMETcut ){ passBaseline = false; passBaselineNoTag = false; passMET = false; }
+        if( metLVec.Pt() < AnaConsts::defaultMETcut ){ passBaseline = false; passBaselineNoTag = false; passMET = false; }
         if( debug ) std::cout<<"met : "<<tr.getVar<double>("met")<<"  defaultMETcut : "<<AnaConsts::defaultMETcut<<"  passBaseline : "<<passBaseline<<std::endl;
 
         // Calculate top tagger related variables. 
@@ -103,7 +102,7 @@ public:
         }
 
         // Pass top tagger requirement?
-        bool passTagger = type3Ptr->passNewTaggerReq();
+        bool passTagger = type3Ptr->passNewTaggerReq() && nTopCandSortedCnt >= AnaConsts::low_nTopCandSortedSel;
 
         if( !passTagger ) passBaseline = false;
 
@@ -146,7 +145,7 @@ public:
         tr.registerDerivedVar("best_had_brJet_mTcomb" + spec, type3Ptr->best_had_brJet_mTcomb);
         tr.registerDerivedVar("best_had_brJet_MT2" + spec, type3Ptr->best_had_brJet_MT2);
 
-        double HT = AnaFunctions::calcHT(tr.getVec<TLorentzVector>("jetsLVec"), AnaConsts::pt50Eta24Arr);
+        double HT = AnaFunctions::calcHT(tr.getVec<TLorentzVector>(jetVecLabel), AnaConsts::pt50Eta24Arr);
         tr.registerDerivedVar("HT" + spec, HT);
 
         if( debug ) std::cout<<"passBaseline : "<<passBaseline<<"  passBaseline : "<<passBaseline<<std::endl;
