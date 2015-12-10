2c1689c5a8d12b5e0d017a11d24902b36d4ea8e0
Scarlet Norberg <scarletnorberg2014@gmail.com>
2015-12-10 15:05:22 -0600

    reworked Pileup code

--- Tools/Weights.h
+++ Tools/Weights.h
@@ -2,29 +2,24 @@
 #define WEIGHTS_H
 
 #include "NTupleReader.h"
-#include "TCanvas.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TChain.h"
 #include "TH1.h"
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <ctime>
-#include <iomanip>
-#include <cmath>
-#include <vector>
+
 
 class Pileup_Sys
 {
- public:
-  Pileup_Sys();
-  virtual ~Pileup_Sys();
-  void getPileup_Sys(NTupleReader& tr);
-  void operator()(NTupleReader& tr);
+public:
+    Pileup_Sys();
+    virtual ~Pileup_Sys();
+    void getPileup_Sys(NTupleReader& tr);
+    void operator()(NTupleReader& tr);
 
- protected:
- private:
+protected:
+
+private: 
+    //need to define the histogram but not fill it till the c file
+    TH1F * pu_central;
+    TH1F * pu_up;
+    TH1F * pu_down;
 
 };
 
