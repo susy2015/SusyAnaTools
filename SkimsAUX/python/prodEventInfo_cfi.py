c5d3aeb784f2b98746b9811cc3788c513a3e4d40
scarletnorberg <scarletnorberg2014@gmail.com>
2016-05-30 17:38:01 -0500

    Update prodEventInfo_cfi.py
    Removing getByLabel

--- SkimsAUX/python/prodEventInfo_cfi.py
+++ SkimsAUX/python/prodEventInfo_cfi.py
@@ -1,8 +1,9 @@
-
 import FWCore.ParameterSet.Config as cms
 
 prodEventInfo = cms.EDFilter(
   "prodEventInfo",
   vtxSrc = cms.InputTag('goodVertices'),
+  puppiSrc = cms.InputTag("slimmedAddPileupInfo"),
+  genSrc= cms.InputTag("generator"),
   debug  = cms.bool(False)
 )
