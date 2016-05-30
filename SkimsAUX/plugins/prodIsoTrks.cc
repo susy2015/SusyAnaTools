9f4dec758df72cadb8de67b11e6450644907173f
scarletnorberg <scarletnorberg2014@gmail.com>
2016-05-30 17:53:28 -0500

    Update prodIsoTrks.cc
    remove getByLabels

--- SkimsAUX/plugins/prodIsoTrks.cc
+++ SkimsAUX/plugins/prodIsoTrks.cc
@@ -1,3 +1,4 @@
+
 #include <memory>
 #include <algorithm>
 
@@ -38,6 +39,7 @@ class prodIsoTrks : public edm::EDFilter {
 
   private:
 
+
     virtual bool filter(edm::Event & iEvent, const edm::EventSetup & iSetup);
 
     edm::InputTag vtxSrc_, metSrc_;
@@ -59,6 +61,20 @@ class prodIsoTrks : public edm::EDFilter {
 
     edm::InputTag genDecayLVec_Src_;
     edm::Handle<std::vector<TLorentzVector> > genDecayLVec_;
+
+    edm::EDGetTokenT<std::vector<int> >W_EmVec_Tok_;
+    edm::EDGetTokenT<std::vector<int> >W_Tau_EmuVec_Tok_;
+    edm::EDGetTokenT<std::vector<int> >W_Tau_ProngsVec_Tok_;
+    edm::EDGetTokenT<std::vector<TLorentzVector> >GenDecayLVec_Tok_;
+    edm::EDGetTokenT<pat::PackedCandidateCollection>PfCandTok_;
+    edm::EDGetTokenT<pat::PackedCandidateCollection> Loose_IsoTrksHandle_Tok_;
+    edm::EDGetTokenT<pat::PackedCandidateCollection> Ref_All_IsoTrksHandle_Tok_;
+    edm::EDGetTokenT<std::vector<double> > Ref_All_IsoTrks_IsoVecHandle_Tok_;
+    edm::EDGetTokenT< std::vector<reco::Vertex> >VtxTok_;
+    edm::EDGetTokenT<edm::View<reco::MET> >MetTok_;
+    edm::EDGetTokenT<std::vector<double> >  Loose_Isotrk_IsoVecHandle_Tok_;
+    edm::EDGetTokenT<std::vector<double> >Loose_Isotrk_DzpvVecHandle_Tok_;
+    edm::EDGetTokenT<pat::PackedCandidateCollection> ForVetoIsoTrks_Tok_;
 
     unsigned int loose_nIsoTrks, nIsoTrksForVeto;
 
@@ -100,8 +116,22 @@ prodIsoTrks::prodIsoTrks(const edm::ParameterSet & iConfig) {
   W_tau_prongsVec_Src_ = iConfig.getParameter<edm::InputTag>("W_tau_prongsVec");
 
   genDecayLVec_Src_ = iConfig.getParameter<edm::InputTag>("genDecayLVec");
-
+  
   debug_       = iConfig.getParameter<bool>("debug");
+
+  W_EmVec_Tok_=consumes<std::vector<int> > (W_emuVec_Src_);
+  W_Tau_EmuVec_Tok_=consumes<std::vector<int> >  (W_tau_emuVec_Src_);
+  W_Tau_ProngsVec_Tok_=consumes<std::vector<int> >(W_tau_prongsVec_Src_);
+  GenDecayLVec_Tok_=consumes<std::vector<TLorentzVector> > (genDecayLVec_Src_);
+  PfCandTok_=consumes<pat::PackedCandidateCollection> (pfCandSrc_);
+  Loose_IsoTrksHandle_Tok_=consumes<pat::PackedCandidateCollection> (loose_isoTrkSrc_);
+  Ref_All_IsoTrksHandle_Tok_=consumes<pat::PackedCandidateCollection> (ref_all_isoTrkSrc_);
+  Ref_All_IsoTrks_IsoVecHandle_Tok_=consumes<std::vector<double> > (ref_all_isoTrk_isoVecSrc_);
+  VtxTok_=consumes< std::vector<reco::Vertex> >(vtxSrc_);
+  MetTok_=consumes<edm::View<reco::MET> >(metSrc_);
+  Loose_Isotrk_IsoVecHandle_Tok_=consumes<std::vector<double> >  (loose_isotrk_isoVecSrc_),
+  Loose_Isotrk_DzpvVecHandle_Tok_=consumes<std::vector<double> >  (loose_isotrk_dzpvVecSrc_);
+  ForVetoIsoTrks_Tok_=consumes<pat::PackedCandidateCollection> (forVetoIsoTrkSrc_);
 
   produces<std::vector<TLorentzVector> >("trksForIsoVetoLVec");
   produces<std::vector<double> >("trksForIsoVetocharge");
@@ -125,7 +155,6 @@ prodIsoTrks::prodIsoTrks(const edm::ParameterSet & iConfig) {
   produces<int>("loosenIsoTrks");
   produces<int>("nIsoTrksForVeto");
 }
-
 
 prodIsoTrks::~prodIsoTrks() {
 }
@@ -155,28 +184,29 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   std::auto_ptr<std::vector<int> > forVetoIsoTrks_idx(new std::vector<int>());
 
   if( !isData_ ){
-     iEvent.getByLabel(W_emuVec_Src_, W_emuVec_);
-     iEvent.getByLabel(W_tau_emuVec_Src_, W_tau_emuVec_);
-     iEvent.getByLabel(W_tau_prongsVec_Src_, W_tau_prongsVec_);
-     iEvent.getByLabel(genDecayLVec_Src_, genDecayLVec_);
+     iEvent.getByToken(W_EmVec_Tok_, W_emuVec_);
+     iEvent.getByToken(W_Tau_EmuVec_Tok_, W_tau_emuVec_);
+     iEvent.getByToken(W_Tau_ProngsVec_Tok_, W_tau_prongsVec_);
+     iEvent.getByToken(GenDecayLVec_Tok_, genDecayLVec_);
   }
 
   edm::Handle< std::vector<reco::Vertex> > vertices;
-  iEvent.getByLabel(vtxSrc_, vertices);
+  iEvent.getByToken(VtxTok_, vertices);
 //  reco::Vertex::Point vtxpos = (vertices->size() > 0 ? (*vertices)[0].position() : reco::Vertex::Point());
 
   edm::Handle<edm::View<reco::MET> > met;
-  iEvent.getByLabel(metSrc_, met);
+  iEvent.getByToken(MetTok_, met);
 
-  iEvent.getByLabel(loose_isoTrkSrc_, loose_isoTrksHandle_); if( loose_isoTrksHandle_.isValid() ) loose_nIsoTrks = loose_isoTrksHandle_->size(); else loose_nIsoTrks =0;
-  iEvent.getByLabel(forVetoIsoTrkSrc_, forVetoIsoTrks_); if( forVetoIsoTrks_.isValid() ) nIsoTrksForVeto = forVetoIsoTrks_->size(); else nIsoTrksForVeto =0;
+  iEvent.getByToken(Loose_Isotrk_IsoVecHandle_Tok_, loose_isoTrksHandle_); if( loose_isoTrksHandle_.isValid() ) loose_nIsoTrks = loose_isoTrksHandle_->size(); else loose_nIsoTrks =0;
+  iEvent.getByToken(ForVetoIsoTrks_Tok_, forVetoIsoTrks_);
+  if( forVetoIsoTrks_.isValid() ) nIsoTrksForVeto = forVetoIsoTrks_->size(); else nIsoTrksForVeto =0;
 
-  iEvent.getByLabel(ref_all_isoTrkSrc_, ref_all_isoTrksHandle_); 
-  iEvent.getByLabel(ref_all_isoTrk_isoVecSrc_, ref_all_isoTrks_isoVecHandle_); 
+  iEvent.getByToken(Ref_All_IsoTrksHandle_Tok_, ref_all_isoTrksHandle_);
+  iEvent.getByToken(Ref_All_IsoTrks_IsoVecHandle_Tok_, ref_all_isoTrks_isoVecHandle_);
 
   edm::Handle<std::vector<double> >  loose_isotrk_isoVecHandle, loose_isotrk_dzpvVecHandle;
-  iEvent.getByLabel(loose_isotrk_isoVecSrc_, loose_isotrk_isoVecHandle);
-  iEvent.getByLabel(loose_isotrk_dzpvVecSrc_, loose_isotrk_dzpvVecHandle);
+  iEvent.getByToken(Loose_Isotrk_IsoVecHandle_Tok_, loose_isotrk_isoVecHandle);
+  iEvent.getByToken(Loose_Isotrk_DzpvVecHandle_Tok_, loose_isotrk_dzpvVecHandle);
   if( loose_isoTrksHandle_.isValid() && loose_isotrk_isoVecHandle.isValid() && loose_isotrk_dzpvVecHandle.isValid() ){
      if( loose_nIsoTrks != loose_isotrk_isoVecHandle->size() || loose_nIsoTrks != loose_isotrk_dzpvVecHandle->size() ){
         std::cout<<"ERROR ... mis-matching between loose_nIsoTrks : "<<loose_nIsoTrks<<"  loose_isotrk_isoVecHandle->size : "<<loose_isotrk_isoVecHandle->size()<<"  loose_isotrk_dzpvVecHandle->size : "<<loose_isotrk_dzpvVecHandle->size()<<std::endl;
@@ -199,7 +229,7 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
      loose_isoTrks_dz->push_back((*loose_isotrk_dzpvVecHandle)[is]);
      loose_isoTrks_pdgId->push_back((*loose_isoTrksHandle_)[is].pdgId());
      loose_isoTrks_iso->push_back((*loose_isotrk_isoVecHandle)[is]);
-     loose_isoTrks_mtw->push_back(mtw);
+      loose_isoTrks_mtw->push_back(mtw);
 
      if( debug_ ){
         std::cout<<"  --> is : "<<is<<"  pt/eta/phi/chg : "<<isoTrkpt<<"/"<<isoTrketa<<"/"<<isoTrkphi<<"/"<<isoTrkcharge<<"  mtw : "<<mtw<<"  pdgId : "<<(*loose_isoTrksHandle_)[is].pdgId()<<"  dz : "<<(*loose_isotrk_dzpvVecHandle)[is]<<"  iso/pt : "<<(*loose_isotrk_isoVecHandle)[is]/isoTrkpt<<std::endl;
@@ -207,7 +237,7 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   }
   if( debug_ ) std::cout<<std::endl;
 
-  iEvent.getByLabel(pfCandSrc_, pfCandHandle_);
+  iEvent.getByToken(PfCandTok_, pfCandHandle_);
   if( pfCandHandle_.isValid() ){
      for(unsigned int ip=0; ip<pfCandHandle_->size(); ip++){
 
@@ -229,7 +259,7 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
            }
         }
 
-        int perCharge = pfCandHandle_->at(ip).charge(); 
+        int perCharge = pfCandHandle_->at(ip).charge();
         if( perCharge ==0 ) continue;
 
         double dz = (*pfCandHandle_)[ip].dz();
@@ -253,14 +283,14 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
               double perdeltaR = perLVec.DeltaR(genLVec);
               if( perdeltaR < isotrk_dR_ ) matched ++;
            }
-   
+
            for(unsigned int ig=0; ig<W_tau_emuVec_->size(); ig++){
               int perIdx = W_tau_emuVec_->at(ig);
               TLorentzVector genLVec = genDecayLVec_->at(perIdx);
               double perdeltaR = perLVec.DeltaR(genLVec);
               if( perdeltaR < isotrk_dR_ ) matched ++;
            }
-   
+
            for(unsigned int ig=0; ig<W_tau_prongsVec_->size(); ig++){
               int perIdx = W_tau_prongsVec_->at(ig);
               TLorentzVector genLVec = genDecayLVec_->at(perIdx);
@@ -268,7 +298,7 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
               if( perdeltaR < isotrk_dR_ ) matched ++;
            }
         }
-
+        
         if( !matched ) continue;
 
         trksForIsoVetoLVec->push_back(perLVec);
@@ -301,12 +331,13 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   std::auto_ptr<int> nIsoTrksForVetoPtr(new int);
   *nIsoTrksForVetoPtr = nIsoTrksForVeto;
-   
+
   iEvent.put(trksForIsoVetoLVec, "trksForIsoVetoLVec");
   iEvent.put(trksForIsoVeto_charge, "trksForIsoVetocharge");
   iEvent.put(trksForIsoVeto_dz, "trksForIsoVetodz");
   iEvent.put(trksForIsoVeto_pdgId, "trksForIsoVetopdgId");
   iEvent.put(trksForIsoVeto_idx, "trksForIsoVetoidx");
+   iEvent.put(trksForIsoVeto_idx, "trksForIsoVetoidx");
   iEvent.put(trksForIsoVeto_iso, "trksForIsoVetoiso");
   iEvent.put(trksForIsoVeto_pfActivity, "trksForIsoVetopfActivity");
 
@@ -329,7 +360,7 @@ bool prodIsoTrks::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
 double prodIsoTrks::GetTrackActivity(const edm::Handle<pat::PackedCandidateCollection> & other_pfcands, const pat::PackedCandidate* track) {
   if (track->pt()<5.) return -1.0;
-  double trkiso(0.); 
+  double trkiso(0.);
   double r_iso = 0.3;
   for (const pat::PackedCandidate &other_pfc : *other_pfcands) {
       if (other_pfc.charge()==0) continue;
