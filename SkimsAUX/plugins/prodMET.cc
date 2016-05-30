8c494fb9dd0fcefea96ef8e1466b891f401f2ad2
scarletnorberg <scarletnorberg2014@gmail.com>
2016-05-30 18:06:33 -0500

    Update prodMET.cc
    change of getByLabel

--- SkimsAUX/plugins/prodMET.cc
+++ SkimsAUX/plugins/prodMET.cc
@@ -1,3 +1,4 @@
+
 #include <memory>
 #include <algorithm>
 
@@ -36,6 +37,8 @@ class prodMET : public edm::EDFilter {
 
     edm::InputTag metSrc_;
     bool debug_;
+    
+    edm::EDGetTokenT<edm::View<pat::MET> >MetTok_;
 
     std::vector<pat::MET::METUncertainty> uncUpList, uncDownList;
 };
@@ -45,6 +48,8 @@ prodMET::prodMET(const edm::ParameterSet & iConfig) {
   metSrc_      = iConfig.getParameter<edm::InputTag>("metSrc");
 
   debug_       = iConfig.getParameter<bool>("debug");
+
+  MetTok_  = consumes<edm::View<pat::MET> >(metSrc_);
 
   uncUpList = {pat::MET::JetResUp, pat::MET::JetEnUp, pat::MET::MuonEnUp, pat::MET::ElectronEnUp, pat::MET::TauEnUp, pat::MET::UnclusteredEnUp, pat::MET::PhotonEnUp};
   uncDownList = {pat::MET::JetResDown, pat::MET::JetEnDown, pat::MET::MuonEnDown, pat::MET::ElectronEnDown, pat::MET::TauEnDown, pat::MET::UnclusteredEnDown, pat::MET::PhotonEnDown};
@@ -65,7 +70,7 @@ prodMET::~prodMET() {
 bool prodMET::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
   edm::Handle<edm::View<pat::MET> > met;
-  iEvent.getByLabel(metSrc_, met);
+  iEvent.getByToken(MetTok_, met);
 
   std::auto_ptr<double> metPtr(new double);
   std::auto_ptr<double> metphiPtr(new double);
@@ -101,3 +106,4 @@ bool prodMET::filter(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 DEFINE_FWK_MODULE(prodMET);
+  
