0cbe4fe35891d33d7008843cd948c25e4a5b2a94
Hongxuan <Hongxuan_Liu@baylor.edu>
2017-01-26 15:39:02 -0600

    fix_a_bug

--- SkimsAUX/workdir/prod/80X_crab_example/MultiCrab3.py
+++ SkimsAUX/workdir/prod/80X_crab_example/MultiCrab3.py
@@ -28,8 +28,8 @@ json_25ns = 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
 #selSubmitKey = 'TEST STATUS TTJets' # 'TEST STATUS': no submission of jobs but rather checking crab job status related to the TTJets. If jobs failed, automatically resubmit them.
 #selSubmitKey = 'TTJets_SingleLeptFrom HTMHT'
 #selSubmitKey = 'TTJets_SingleLeptFrom TTJets_DiLept'
-selSubmitKey = 'TEST HTMHT TTJets_SingleLeptFrom TTJets_DiLept'
-#selSubmitKey = 'MET TTJets_SingleLeptFrom TTJets_DiLept SMS-T1tttt_FastSim_scan'
+#selSubmitKey = 'TEST HTMHT TTJets_SingleLeptFrom TTJets_DiLept'
+selSubmitKey = 'MET TTJets_SingleLeptFrom TTJets_DiLept SMS-T1tttt_FastSim_scan'
 #selSubmitKey = 'TEST STATUS TTJets_SingleLeptFrom HTMHT'
 #selSubmitKey = 'TEST ALL'
 #selSubmitKey = 'TEST TTJets_SingleLeptFrom TTJets_Inc TTJets_DiLept ZJetsToNuNu_HT'
@@ -222,8 +222,7 @@ jobslist = {
     'SMS-T2tt_FastSim_scan_400to1200'        : [False, '/SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM', 1],
     'SMS-T5ttcc_FastSim_scan'                : [False, '/SMS-T5ttcc_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v3/MINIAODSIM', 1],
     'SMS-T5tttt_dM175_FastSim_scan'          : [False, '/SMS-T5tttt_dM175_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM', 1],
-    'SMS-T1ttbb_FastSim_scan'                : [False, '/SMS-T1ttbb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
-', 1],
+    'SMS-T1ttbb_FastSim_scan'                : [False, '/SMS-T1ttbb_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16Fast_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM', 1],
 
     # Data
     'HTMHT-Run2016H-PromptReco-v3'          : [True, '/HTMHT/Run2016H-PromptReco-v3/MINIAOD', 10],
