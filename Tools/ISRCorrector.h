88c77210e7a5bcd6d1170ec9c98d55bd54c74bfc
Nadja <nadja.strobbe@cern.ch>
2017-02-08 23:59:29 -0600

    additional stuff for ISR weights

--- Tools/ISRCorrector.h
+++ Tools/ISRCorrector.h
@@ -14,7 +14,7 @@ private:
 
  public:
   //constructor
- ISRCorrector(std::string fnISR = "Signal_fastsim_T1tttt_scan_ISR.root", std::string ISRJetsFilePath = "ISR_Root_Files", TString massPoint = "1200_800"): massPoint_(massPoint)
+ISRCorrector(std::string fnISR = "Signal_fastsim_T1tttt_scan_ISR.root", std::string ISRJetsFilePath = "ISR_Root_Files", TString massPoint = "1200_800", std::string suffix = "TTbarSingleLepT"): massPoint_(massPoint)
   {
       isFirst = true;
       
@@ -30,16 +30,20 @@ private:
 	}
 
       if( massPoint.IsNull() ){
-         h_njetsisr = (TH1D*)fnISR_->Get("NJetsISR");
-      }else{
+          if(suffix !="")
+              h_njetsisr = (TH1D*)fnISR_->Get(("NJetsISR_"+suffix).c_str());
+          else {
+              h_njetsisr = (TH1D*)fnISR_->Get("NJetsISR");
+          }
+      } else{
          h_njetsisr = (TH1D*)fnISR_->Get("NJetsISR_"+ massPoint_);
       }
 
 
       //get Weight histograms.
-      TH1D *h_isr_up = (TH1D*)fISRWght->Get("isr_weights_up");
-      TH1D *h_isr_central= (TH1D*)fISRWght->Get("isr_weights_central");
-      TH1D *h_isr_down = (TH1D*)fISRWght->Get("isr_weights_down");
+      h_isr_up = (TH1D*)fISRWght->Get("isr_weights_up");
+      h_isr_central= (TH1D*)fISRWght->Get("isr_weights_central");
+      h_isr_down = (TH1D*)fISRWght->Get("isr_weights_down");
 
       //Set Weight
       SetWeights_Up(h_isr_up,  h_njetsisr);
@@ -64,14 +68,33 @@ private:
 
   void operator()(NTupleReader& tr);
 
-
-
-
+  void resetSample(std::string newsample) 
+  {
+      suffix = newsample;
+      
+      TH1D* h_njetsisr2 = (TH1D*)fnISR_->Get(("NJetsISR_"+newsample).c_str());
+      if(h_njetsisr2)
+      {
+          h_njetsisr = h_njetsisr2;
+          SetWeights_Up(h_isr_up,  h_njetsisr);
+          SetWeights_Cent(h_isr_central,  h_njetsisr);
+          SetWeights_Down(h_isr_down,  h_njetsisr);
+      }
+      else 
+      {
+          h_njetsisr = (TH1D*)fnISR_->Get("NJetsISR_TTbarSingleLepT");
+          SetWeights_Up(h_isr_up,  h_njetsisr);
+          SetWeights_Cent(h_isr_central,  h_njetsisr);
+          SetWeights_Down(h_isr_down,  h_njetsisr);
+      }
+  }
 
   //member variables
   TH1D *h_njetsisr, *h_weights_central, *h_weights_up, *h_weights_down;
+  TH1D *h_isr_up, *h_isr_central, *h_isr_down;
   TFile *fnISR_,  *fISRWght;
   TString massPoint_;
+  std::string suffix;
 };
 
 
