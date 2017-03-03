87bc58ebe95c6a8272ef44004c784a3184618cb5
Hongxuan <Hongxuan_Liu@baylor.edu>
2017-03-03 17:10:12 -0600

    Configuration changes for re-MINIAOD processing

--- SkimsAUX/python/prodMET_cfi.py
+++ SkimsAUX/python/prodMET_cfi.py
@@ -4,5 +4,6 @@ import FWCore.ParameterSet.Config as cms
 prodMET = cms.EDFilter(
   "prodMET",
   metSrc = cms.InputTag('slimmedMETs'),
-  debug  = cms.bool(False)
+  debug  = cms.bool(False),
+  addcalomet = cms.bool(True)
 )
