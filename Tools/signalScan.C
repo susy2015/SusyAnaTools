33fb3411404d7219375a9e94d3a5dfdf191c85f1
Nathaniel <pastika@cern.ch>
2017-03-06 13:46:56 -0600

    Remove heppy dependence

--- Tools/signalScan.C
+++ Tools/signalScan.C
@@ -39,7 +39,7 @@
 
 #include "../TopTagger/interface/indexSort.h"
 
-#include "../SkimsAUX/plugins/MT2CalcCore.h"
+//#include "../SkimsAUX/plugins/MT2CalcCore.h"
 
 #include "ISRCorrector.h"
 
@@ -52,7 +52,7 @@ const bool doSingleMuonCS = false;
 const bool doInvDphi = false;
 const bool usegenmet = false;
 
-MT2CalcCore * mt2Calc;
+//MT2CalcCore * mt2Calc;
 
 BaselineVessel * SRblv =0;
 std::string spec = "MY";
@@ -965,7 +965,7 @@ public:
 
        if( passNoiseEventFilter && passFastsimEventFilter && passMuonVeto && passEleVeto && passIsoTrkVeto && passnJets && passBJets && passHT && passMET && passdPhis && passTagger ){
           TLorentzVector metLVec; metLVec.SetPtEtaPhiM(met, 0, metphi, 0);
-          const double alt_MT2 = mt2Calc->getMT2Hemi(tr.getVec<TLorentzVector>("jetsLVec_forTagger" + spec), metLVec);
+          const double alt_MT2 = -99909;//mt2Calc->getMT2Hemi(tr.getVec<TLorentzVector>("jetsLVec_forTagger" + spec), metLVec);
 
           loose_MT2_vs_met_->Fill(met, best_had_brJet_MT2, weight);
           loose_alt_MT2_vs_met_->Fill(met, alt_MT2, weight);
@@ -1794,7 +1794,7 @@ void anaFunc(NTupleReader *tr, std::vector<TTree *> treeVec, const std::vector<s
 
 void signalScan(int argc, char *argv[]){
 
-   mt2Calc = new MT2CalcCore();
+    //mt2Calc = new MT2CalcCore();
 
    AnaFunctions::prepareForNtupleReader();
 
