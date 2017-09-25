2dfd6e8af00cc62a3b6ab9d608002152820b23d4
Nathaniel <pastika@cern.ch>
2017-09-25 11:01:10 -0500

    Added regexp matching to make script more general

--- Tools/condor/batchList.py
+++ Tools/condor/batchList.py
@@ -1,9 +1,9 @@
 import os
 import optparse
 from glob import glob
+import re
 
 def eoscp(filename, path, force = False, eosurl = "root://cmseos.fnal.gov"):
-    stripedName = filename.split("/")[-1]
     if force:
         command = "xrdcp --force %s %s/%s"%(filename, eosurl, "/".join([path, filename]))
     else:
@@ -16,25 +16,31 @@ def eoscp(filename, path, force = False, eosurl = "root://cmseos.fnal.gov"):
 def eosls(path, option = "", eosurl = "root://cmseos.fnal.gov"):
     return os.popen("xrdfs %s ls %s %s"%(eosurl, option, path))
 
-def recSearch(path, fout = None, eosurl = "root://cmseos.fnal.gov"):
+def recSearch(path, fout = None, eosurl = "root://cmseos.fnal.gov", match=".*\.root", reject=".*failed.*"):
     with eosls(path, "-l", eosurl = options.eosurl) as stdout:
         for l in stdout:
+            #if this is a directory dive in to continue searching
             if l.startswith("d"):
                 endpath = l.split()[-1].split("/")[-1]
-                recSearch("/".join([path, endpath]), fout)
+                recSearch("/".join([path, endpath]), fout, eosurl=eosurl, match=match, reject=reject)
             else:
                 filename = "/".join([path, l.split()[-1].split("/")[-1]])
-                if filename.endswith(".root") and not "failed" in filename:
+                #if filename.endswith(".root") and not "failed" in filename:
+                if re.match(match, filename) and not re.match(reject, filename):
                     if fout == None:
                         print "".join(["%s/"%eosurl, filename])
                     else:
-                        #fout.write("".join(["%s/"%eosurl, filename, "\n"]))
                         fp = "".join(["%s/"%eosurl, filename, "\n"])
                         fpsplit = fp.split("/")
+                        #get part of file list before data folder
                         first = "/".join(fpsplit[:-3])
+                        #get date folder
                         second = fpsplit[-3]
+                        #get portion of path after date folder
                         third = "/".join(fpsplit[-2:])
+                        #construct key which is unique to each file (but not date)
                         key = (first, third, )
+                        #for each file only keep the results from the latest dated folder
                         if key in fout:
                             if(second > fout[key]):
                                 fout[key] = second
@@ -46,12 +52,14 @@ if __name__ == "__main__":
 
     parser = optparse.OptionParser("usage: %prog [options]\n")
     
-    parser.add_option ('-d',      dest='directory',  type='string',   default = "",    help="file path to begin")
-    parser.add_option ('-f',      dest='file',       type='string',   default = "",    help="File name to transfer")
-    parser.add_option ('-e',      dest='eosurl',     type='string',   default = "root://cmseos.fnal.gov", help="eos url (Defaults to root://cmseos.fnal.gov)")
-    parser.add_option ('-l',      dest='list',   action="store_true", default = False, help="Create file lists")
-    parser.add_option ('-c',      dest='copy',   action="store_true", default = False, help="Copy a file to eos")
-    parser.add_option ('--force', dest='force',  action="store_true", default = False, help="Force overwrite when doing xrdcp (use with caution!)")
+    parser.add_option ('-d',      dest='directory',  type='string',   default = "",                           help="file path to begin")
+    parser.add_option ('-f',      dest='file',       type='string',   default = "",                           help="File name to transfer")
+    parser.add_option ('-e',      dest='eosurl',     type='string',   default = "root://cmseos.fnal.gov",     help="eos url (Defaults to root://cmseos.fnal.gov)")
+    parser.add_option ('-m',      dest='match',      type='string',   default = ".*stopFlatNtuples.*\.root$", help="file paths must match this regular expression")
+    parser.add_option ('-r',      dest='reject',     type='string',   default = ".*failed.*",                 help="file paths must NOT match this regular expression")
+    parser.add_option ('-l',      dest='list',   action="store_true", default = False,                        help="Create file lists")
+    parser.add_option ('-c',      dest='copy',   action="store_true", default = False,                        help="Copy a file to eos")
+    parser.add_option ('--force', dest='force',  action="store_true", default = False,                        help="Force overwrite when doing xrdcp (use with caution!)")
     
     options, args = parser.parse_args()
     
@@ -60,13 +68,14 @@ if __name__ == "__main__":
     if options.list:
         with eosls(startPath, "-l", eosurl = options.eosurl) as stdout:
             for l in stdout:
+                #if this is a directory, begin recursive search for files
                 if l.startswith("d"):
                     endpath = l.split()[-1].split("/")[-1]
                     print "".join([endpath, ".txt"])
                     fileName = "".join([endpath, ".txt"])
                     f = open(fileName, "w")
                     outDict = {}
-                    recSearch("/".join([startPath, endpath]), outDict, eosurl = options.eosurl)
+                    recSearch("/".join([startPath, endpath]), outDict, eosurl = options.eosurl, match=options.match, reject=options.reject)
                     for key in outDict:
                         f.write("/".join([key[0], outDict[key], key[1]]))
                     f.close()
