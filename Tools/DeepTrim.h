763f881d13183d73765636829a2d73f1882c7d33
Nathaniel <pastika@cern.ch>
2017-10-04 00:10:43 -0500

    Added updated version of deepTrim script from Hua

---
+++ Tools/DeepTrim.h
@@ -0,0 +1,21 @@
+#include <string>
+
+size_t find_Nth
+(
+  const std::string & str ,   // where to work
+  unsigned            N ,     // N'th ocurrence
+  const std::string & find    // what to 'find'
+)
+{
+  if ( 0==N ) { return std::string::npos; }
+  size_t pos,from=0;
+  unsigned i=0;
+  while ( i<N )
+  {
+    pos=str.find(find,from);
+    if ( std::string::npos == pos ) { break; }
+    from = pos + 1; // from = pos + find.size();
+    ++i;
+  }
+  return pos;
+}
