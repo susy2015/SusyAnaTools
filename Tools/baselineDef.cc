4995f99be9316908774b981491a9f353c14f6c7f
Nathaniel <pastika@cern.ch>
2018-04-20 14:26:48 -0500

    Updated to work with the lastest version of the top tagging code

--- Tools/baselineDef.cc
+++ Tools/baselineDef.cc
@@ -526,7 +526,7 @@ AK8Flag BaselineVessel::FlagAK8FromCSV(Constituent &ak8) const
   {
     for(unsigned int ij=0; ij<jets.size(); ij++)
     {
-      if (sub.DeltaR(jets.at(ij)) < 0.4)
+      if (sub.p().DeltaR(jets.at(ij)) < 0.4)
       {
         if (jets.at(ij).Pt() < 20 || fabs(jets.at(ij).Eta()) > 2.4) continue;
         if (CSV.at(ij) > AnaConsts::cutCSVS ) mediumbcnt ++;
@@ -581,7 +581,7 @@ AK8Flag BaselineVessel::FlagAK8FromTagger(Constituent &ak8 )
       {
         for(auto sub : ak8.getSubjets())
         {
-          if (tri.DeltaR(sub)<0.4)
+          if (tri.DeltaR(sub.p())<0.4)
           {
             return WinTopTag;
           }
@@ -740,7 +740,7 @@ std::vector<TLorentzVector>  BaselineVessel::GetAK4NoSubjet(Constituent &ak8, st
     bool ismatched = false;
     for(auto sub : ak8.getSubjets())
     {
-      if (ak4.DeltaR(sub)<0.4)
+      if (ak4.DeltaR(sub.p())<0.4)
       {
         ismatched = true;
         break;
