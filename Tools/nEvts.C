f681a3ae381e3f503ed44f71a1e37cc91ce75283
Christopher_Madrid <Chris_Madrid@Baylor.edu>
2018-04-25 17:03:59 -0500

    changing config names to match stop groups

--- Tools/nEvts.C
+++ Tools/nEvts.C
@@ -36,8 +36,8 @@ int main(int argc, char *argv[])
   if(getNegWeights)
   std::cout << "Will compute negative weight fraction" << std::endl;
     
-  AnaSamples::SampleSet        ss("sampleSets.cfg");
-  AnaSamples::SampleCollection sc("sampleCollections.cfg", ss);
+  AnaSamples::SampleSet        ss("sampleSets.txt");
+  AnaSamples::SampleCollection sc("sampleCollections.txt", ss);
 
   std::string selKeyStr;
   if(argc >= optind+1)
