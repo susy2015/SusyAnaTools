ce3cef07f2da9f2b3d401edac5f47ffdf670610a
caleb-james-smith <caleb_smith2@baylor.edu>
2018-09-20 16:06:48 -0500

    Updates to nEvts.C from Joe.

--- Tools/nEvts.C
+++ Tools/nEvts.C
@@ -4,6 +4,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TChain.h"
+#include "TH1.h"
 
 #include <iostream>
 #include <sstream>
@@ -11,95 +12,79 @@
 
 int main(int argc, char *argv[])
 {
-  int opt;
-  int option_index = 0;
-  static struct option long_options[] = {
-      {"negw",     no_argument, 0, 'w'},
-      {"skipData", no_argument, 0, 's'}
-  };
+    int opt;
+    int option_index = 0;
+    static struct option long_options[] = {
+        {"negw",     no_argument, 0, 'w'},
+        {"skipData", no_argument, 0, 's'}
+    };
 
-  bool getNegWeights = false, skipData = false;
-  while((opt=getopt_long(argc, argv, "ws", long_options, &option_index)) != -1)
-  {
-    switch(opt)
+    bool getNegWeights = false, skipData = false;
+    while((opt=getopt_long(argc, argv, "ws", long_options, &option_index)) != -1)
     {
-      case 'w':
-      getNegWeights = true;
-      break;
+        switch(opt)
+        {
+        case 'w':
+            getNegWeights = true;
+            break;
 
-      case 's':
-      skipData = true;
-      break;
+        case 's':
+            skipData = true;
+            break;
+        }
     }
-  }
   
-  if(getNegWeights)
-  std::cout << "Will compute negative weight fraction" << std::endl;
-    
-  AnaSamples::SampleSet        ss("sampleSets.cfg");
-  AnaSamples::SampleCollection sc("sampleCollections.cfg", ss);
-
-  std::string selKeyStr;
-  if(argc >= optind+1)
-  {
-    selKeyStr = argv[optind];
-    std::cout << "selKeyStr : " << selKeyStr << std::endl;
-  }
-
-  std::stringstream ssSelKey(selKeyStr);
-  std::string buf;
-  std::vector<std::string> keyStrVec;
-    
-  while(ssSelKey >> buf) keyStrVec.push_back(buf);
-
-  for(auto& file : ss) 
-  {
-    if( !keyStrVec.empty() )
-    {
-      bool found = false;
-      for(auto& str : keyStrVec ){ if( file.first.find(str) !=std::string::npos ) found = true; }
-      if( !found ) continue;
-    }
-    if(skipData && file.first.find("Data") != std::string::npos)
-    {
-        std::cout << "Skipping " << file.first << std::endl;
-        continue;
-    }
-
-    TChain *t = new TChain(file.second.treePath.c_str());
-    file.second.addFilesToChain(t);
-    
-    int nEntries = t->GetEntries();
-    if(nEntries != file.second.nEvts)
-        std::cout << "DIFFERENT:\t";
-    std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << nEntries << "\twas: " << file.second.nEvts  << std::endl;
-
     if(getNegWeights)
-    {
-      NTupleReader tr = NTupleReader(t);
+        std::cout << "Will compute negative weight fraction" << std::endl;
     
-      int negw = 0;
-      int posw = 0;
-      while(tr.getNextEvent())
-      {
-        float stored_weight = 0;
-        if( tr.checkBranch("stored_weight") )
-        {
-          stored_weight = tr.getVar<float>("stored_weight");
-        }
-        else if( tr.checkBranch("Weight") )
-        {
-          stored_weight = tr.getVar<float>("Weight");
-        }
-        else
-        {
-          std::cout<<"No weight information in NTupleReader object"<<std::endl;
-          break;
-        }
-        if(stored_weight > 0) ++posw;
-        else ++negw;
-      }
-      std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << "Neg weigths = " << negw << ", Pos weights = " << posw << std::endl;
+    AnaSamples::SampleSet        ss("sampleSets.cfg");
+    AnaSamples::SampleCollection sc("sampleCollections.cfg", ss);
+
+    std::string selKeyStr;
+    if(argc >= optind+1)
+    {
+        selKeyStr = argv[optind];
+        std::cout << "selKeyStr : " << selKeyStr << std::endl;
     }
-  }   
+
+    std::stringstream ssSelKey(selKeyStr);
+    std::string buf;
+    std::vector<std::string> keyStrVec;
+    
+    for(auto& file : ss) 
+    {
+        if( !keyStrVec.empty() )
+        {
+            bool found = false;
+            for(auto& str : keyStrVec ){ if( file.first.find(str) !=std::string::npos ) found = true; }
+            if( !found ) continue;
+        }
+        if(skipData && file.first.find("Data") != std::string::npos)
+        {
+            std::cout << "Skipping " << file.first << std::endl;
+            continue;
+        }
+
+        TChain *t = new TChain(file.second.treePath.c_str());
+        file.second.addFilesToChain(t);
+    
+        //int nEntries = t->GetEntries();
+        //if(nEntries != file.second.nEvts)
+        //    std::cout << "DIFFERENT:\t";
+        //std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << nEntries << "\twas: " << file.second.nEvts  << std::endl;
+
+        if(getNegWeights)
+        {
+
+            TH1* h3 = new TH1D("h3", "h3", 2, -100000, 100000);
+            
+            t->Draw("stored_weight>>h3", "stored_weight", "goff");
+            std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << "Neg weigths = " << int(h3->GetBinContent(1)) << ", Pos weights = " << int(h3->GetBinContent(2)) << std::endl;
+            // delete TH1* to avoid memory leaks / save memory / not crash / be safe
+            delete h3;
+        }
+        // delete TChain* to avoid memory leaks / save memory / not crash / be safe
+        delete t;
+    }   
 }
+
