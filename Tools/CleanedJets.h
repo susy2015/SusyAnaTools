7e2a2d7505790fb98e5228efa38116e9ad7ae67c
caleb-james-smith <caleb_smith2@baylor.edu>
2018-10-22 12:17:32 -0500

    CleanedJets module. Should support dR object cleaning from jets (photon, lepton, etc)

---
+++ Tools/CleanedJets.h
@@ -0,0 +1,43 @@
+// CleanedJets.h
+//
+// Caleb J. Smith
+// October 22, 2018
+
+// Input vector of TLorentzVector: objects (photons, leptons, etc)
+// Input vector of TLorentzVector: jets (before they are clean)
+// Return vector of TLorentzVector: cleaned jets (dR cleaning)
+
+
+#ifndef CLEANEDJETS_H
+#define CLEANEDJETS_H
+
+#include "SusyAnaTools/Tools/NTupleReader.h"
+#include "Math/VectorUtil.h"
+
+#include <vector>
+#include <iostream>
+#include <string>
+
+#endif
+
+class CleanedJets {
+
+private:
+
+  void generateCleanedJets(NTupleReader& tr) {
+    const auto& gammaLVec = tr.getVec<TLorentzVector>("gammaLVec"); // reco photon
+    double htWithoutPhoton = 0.0;
+    tr.registerDerivedVar("htWithoutPhoton", htWithoutPhoton);
+  }
+public:
+
+  CleanedJets(){}
+
+  ~CleanedJets(){}
+
+  void operator()(NTupleReader& tr)
+  {
+    generateCleanedJets(tr);
+  }
+};
+
