1dc980f01b54a0d81e7e31baaa940232daa26e21
caleb-james-smith <caleb_smith2@baylor.edu>
2018-10-23 14:51:23 -0500

    Create new jet collections with photons removed.

--- Tools/CleanedJets.h
+++ Tools/CleanedJets.h
@@ -12,6 +12,7 @@
 #define CLEANEDJETS_H
 
 #include "SusyAnaTools/Tools/NTupleReader.h"
+#include "SusyAnaTools/Tools/customize.h"
 #include "Math/VectorUtil.h"
 
 #include <vector>
@@ -25,12 +26,38 @@ class CleanedJets {
 private:
 
   void generateCleanedJets(NTupleReader& tr) {
-    const auto& gammaLVec = tr.getVec<TLorentzVector>("gammaLVec"); // reco photon
-    double htWithoutPhoton = 0.0;
-    int match = -1;
-    match = AnaFunctions::jetObjectdRMatch(photon, jetsLVec, dRMax);
+    const auto& gammaLVec               = tr.getVec<TLorentzVector>("gammaLVec");               // reco photon
+    const auto& jetsLVec                = tr.getVec<TLorentzVector>("jetsLVec");                // jets
+    const auto& prodJetsNoLep_jetsLVec  = tr.getVec<TLorentzVector>("prodJetsNoLep_jetsLVec");  // lepton cleaned jets
+        
+    auto* jetsNoPhotonLVec          = new std::vector<TLorentzVector>();
+    auto* jetsNoPhotonNoLeptonLVec  = new std::vector<TLorentzVector>();
 
-    tr.registerDerivedVar("htWithoutPhoton", htWithoutPhoton);
+    const float dRMax = 0.15; // dR between photon and jet
+
+    // loop over photons
+    // determine which jets to keep
+    std::vector<bool> keepJet1(jetsLVec.size(), true);
+    std::vector<bool> keepJet2(prodJetsNoLep_jetsLVec.size(), true);
+    for (int i = 0; i < gammaLVec.size(); ++i)
+    {
+        //jetObjectdRMatch(const TLorentzVector& object, const std::vector<TLorentzVector>& jetsLVec, const float jetObjectdRMax)
+        int match1 = AnaFunctions::jetObjectdRMatch(gammaLVec[i], jetsLVec, dRMax);
+        int match2 = AnaFunctions::jetObjectdRMatch(gammaLVec[i], prodJetsNoLep_jetsLVec, dRMax);
+        if (match1 >= 0) keepJet1[match1] = false;
+        if (match2 >= 0) keepJet2[match2] = false;
+    }
+    for (int i = 0; i < jetsLVec.size(); ++i)
+    {
+        if (keepJet1[i]) jetsNoPhotonLVec->push_back(jetsLVec[i]);
+    }
+    for (int i = 0; i < prodJetsNoLep_jetsLVec.size(); ++i)
+    {
+        if (keepJet1[i]) jetsNoPhotonNoLeptonLVec->push_back(prodJetsNoLep_jetsLVec[i]);
+    }
+
+    tr.registerDerivedVec("jetsNoPhotonLVec",           jetsNoPhotonLVec);
+    tr.registerDerivedVec("jetsNoPhotonNoLeptonLVec",   jetsNoPhotonNoLeptonLVec);
   }
 public:
 
