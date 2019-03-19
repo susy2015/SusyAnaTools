6fe1f0101cddf59569b5d8e73e21facf0a05cae3
caleb-james-smith <caleb_smith2@baylor.edu>
2019-03-19 14:18:36 -0500

    Implement checkBranch() function to check for branch containing weights. Added error checking for getting tree, branches, and weight variable.

--- Tools/nEvts.C
+++ Tools/nEvts.C
@@ -10,18 +10,28 @@
 #include <sstream>
 #include <getopt.h>
 
+bool checkBranch(TChain* t, const char* name)
+{
+    TBranch* br = (TBranch*)t->GetListOfBranches()->FindObject(name);
+    return bool(br);
+}
+
 int main(int argc, char *argv[])
 {
     int opt;
     int option_index = 0;
     static struct option long_options[] = {
-        {"skipData", no_argument, 0, 's'},
-        {"legacy",   no_argument, 0, 'l'}
+        {"skipData",                no_argument, 0, 's'},
+        {"legacy",                  no_argument, 0, 'l'},
+        {"sampleCollectionsFile",   required_argument, 0, 'C'},
+        {"sampleSetsFile",          required_argument, 0, 'S'}
     };
 
     bool skipData = false;
     bool legacy = false;
-    while((opt=getopt_long(argc, argv, "sl", long_options, &option_index)) != -1)
+    std::string sampleCollectionsFile = "";
+    std::string sampleSetsFile = "";
+    while((opt=getopt_long(argc, argv, "slC:S:", long_options, &option_index)) != -1)
     {
         switch(opt)
         {
@@ -31,12 +41,32 @@ int main(int argc, char *argv[])
         case 'l':
             legacy = true;
             break;
+        case 'C':
+            sampleCollectionsFile = optarg;
+            break;
+        case 'S':
+            sampleSetsFile = optarg;
+            break;
         }
     }
-  
+   
+    // check for sample collections and sample sets files
+    if ( sampleCollectionsFile.empty() )
+    {
+        printf("Please provide the name of the sample collections file using the -C option.\n");
+        return 1;
+    }
+    if ( sampleSetsFile.empty() )
+    {
+        printf("Please provide the name of the sample sets file using the -S option.\n");
+        return 1;
+    }
     
-    AnaSamples::SampleSet        ss("sampleSets_2016.cfg");
-    AnaSamples::SampleCollection sc("sampleCollections.cfg", ss);
+    printf("sample collections file: %s\n", sampleCollectionsFile.c_str());
+    printf("sample sets file: %s\n", sampleSetsFile.c_str());
+  
+    AnaSamples::SampleSet        ss(sampleSetsFile);
+    AnaSamples::SampleCollection sc(sampleCollectionsFile, ss);
 
     std::string selKeyStr;
     if(argc >= optind+1)
@@ -67,29 +97,53 @@ int main(int argc, char *argv[])
         }
         if(skipData && file.first.find("Data") != std::string::npos)
         {
-            std::cout << "Skipping " << file.first << std::endl;
+            std::cout << "Skipping Data sample " << file.first << std::endl;
             continue;
         }
 
         TChain *t = new TChain(file.second.treePath.c_str());
+        
+        // check that tree exists
+        if (!t)
+        {
+            printf("ERROR for %s: Unable to load tree \"%s\". Skipping %s.\n", file.first.c_str(), file.second.treePath.c_str(), file.first.c_str());
+            continue;
+        }
+        
         file.second.addFilesToChain(t);
+        
+        // check that branches exist
+        TObjArray* objArray = t->GetListOfBranches();
+        if (! objArray)
+        {
+            printf("ERROR for %s: Unable to get list of branches from tree \"%s\". Skipping %s.\n", file.first.c_str(), file.second.treePath.c_str(), file.first.c_str());
+            continue;
+        }
     
-        //int nEntries = t->GetEntries();
-        //if(nEntries != file.second.nEvts)
-        //    std::cout << "DIFFERENT:\t";
-        //std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << nEntries << "\twas: " << file.second.nEvts  << std::endl;
-
         TH1* h3 = new TH1D("h3", "h3", 2, -100000, 100000);
         
+        // --- Calculate weights --- //
+        //
+        // weight name for prod2016MCv2_NANO_Skim ntuples: genWeight 
         // weight name for CMSSW8028_2016 ntuples: stored_weight           
-        if (legacy) 
+        // weight name for Stealth SUSY ntuples: Weight
+        
+        if(checkBranch(t, "genWeight"))
+        {
+            t->Draw("genWeight>>h3", "1", "goff");
+        }
+        else if(checkBranch(t, "stored_weight"))
         {
             t->Draw("stored_weight>>h3", "1", "goff");
         }
-        // weight name for prod2016MCv2_NANO_Skim ntuples: genWeight 
+        else if(checkBranch(t, "Weight"))
+        {
+            t->Draw("Weight>>h3", "1", "goff");
+        }
         else
         {
-            t->Draw("genWeight>>h3", "1", "goff");
+            printf("ERROR for %s: No weight variable found tree \"%s\". Skipping %s.\n", file.first.c_str(), file.second.treePath.c_str(), file.first.c_str());
+            continue;
         }
         
         std::cout << "Processing file(s): " << file.second.tag << "\t" << file.second.filePath + file.second.fileName << "\t" << "Neg weigths = " << int(h3->GetBinContent(1)) << ", Pos weights = " << int(h3->GetBinContent(2)) << std::endl;
