69d06a770199da8eedb85e0f8516c5cf428dc0e9
Nathaniel <pastika@cern.ch>
2019-04-03 17:26:54 -0500

    add missing wrapper script

---
+++ Tools/condor/goNEvts.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+_PWD=${PWD}
+#_CONDOR_SCRATCH_DIR=${PBS_O_WORKDIR}
+
+printenv
+
+echo ""
+
+ls 
+
+echo ""
+
+export PATH=${PATH}:/cvmfs/cms.cern.ch/common
+export CMS_PATH=/cvmfs/cms.cern.ch
+
+#move the files from /home to /var/tmp
+mv ${_PWD}/$2.tar.gz ${_CONDOR_SCRATCH_DIR}
+mv ${_PWD}/TT.tar.gz ${_CONDOR_SCRATCH_DIR}
+
+cd ${_CONDOR_SCRATCH_DIR}
+
+#get the release setup and in place
+tar -xzf $2.tar.gz
+cd $2/
+mkdir -p src
+cd src
+scram b ProjectRename
+eval `scramv1 runtime -sh`
+
+#set up local code
+tar -xzf ${_CONDOR_SCRATCH_DIR}/TT.tar.gz
+cd WORLDSWORSESOLUTIONTOAPROBLEM
+
+mkdir -p ../obj
+mv samplesModule.so ../obj
+
+pwd
+ls -lhrt
+
+python nEvts.py -s sampleSets.cfg -d "^$1$" > output_$1.txt
+ls -lhrt
+
+mv output_$1.txt ${_CONDOR_SCRATCH_DIR}
+
+rm $(echo $6 | sed 's|.*/||')
+rm -r ${_CONDOR_SCRATCH_DIR}/$2
