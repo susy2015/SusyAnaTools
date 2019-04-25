f21f69188c5544ef51302138db89867fbe7ba5b1
caleb-james-smith <caleb_smith2@baylor.edu>
2019-04-24 19:28:27 -0500

    Add feature to replace cross sections in config file give config file with correct cross sections.

--- Tools/condor/updateSamples.py
+++ Tools/condor/updateSamples.py
@@ -22,6 +22,27 @@ class TextColor:
     blue = "\033[94m"
     end = "\033[0m"
 
+def updateXSection(sample, xsection_dict):
+    # be careful: strip() removes spaces and endlines
+    sample_list = list(x.strip() for x in sample.split(','))
+    name = sample_list[0] 
+    if name[0] == "#":
+        name = name[1:].strip()
+    
+    new_xsection = -1.0
+    newSample = ", ".join(sample_list) + "\n"
+    
+    try:
+        new_xsection = xsection_dict[name]
+    except KeyError:
+        print "KeyError for name: {0}".format(name)
+        return newSample
+
+    sample_list[4] = new_xsection
+    newSample = ", ".join(sample_list) + "\n"
+    return newSample
+
+
 def getNewSample(sample, weight_dict, neventsFile):
     # be careful: strip() removes spaces and endlines
     sample_list = list(x.strip() for x in sample.split(','))
@@ -38,13 +59,13 @@ def getNewSample(sample, weight_dict, neventsFile):
         return newSample
     try:
         old_neg_weights = int(float(sample_list[-2]))
-    except:
+    except ValueError:
         print TextColor.red + "ERROR: sample_list format is not correct; {0} is not a number".format(sample_list[-2]) + TextColor.end
         print "       Skipping sample_list = {0}".format(sample_list)
         return newSample
     try:
         old_pos_weights = int(float(sample_list[-3]))
-    except:
+    except ValueError:
         print TextColor.red + "ERROR: sample_list format is not correct; {0} is not a number".format(sample_list[-3]) + TextColor.end
         print "       Skipping sample_list = {0}".format(sample_list)
         return newSample
@@ -54,8 +75,8 @@ def getNewSample(sample, weight_dict, neventsFile):
         neventsFile.seek(0)
         num_matches = 0
         message = ""
-        for nevent in neventsFile:
-            match = regex.match(nevent)
+        for line in neventsFile:
+            match = regex.match(line)
             if not match:
                 continue
             num_matches += 1
@@ -110,50 +131,81 @@ def getNewSample(sample, weight_dict, neventsFile):
 def main():
     # options
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--nevents_file",    "-e", default="",                     help="file containing number of events with weights")
-    parser.add_argument("--input_file",      "-i", default="../sampleSets.cfg",    help="Existing SampleSets.cfg file")
-    parser.add_argument("--output_file",     "-o", default="../sampleSets_v1.cfg", help="New SampleSets.cfg file to create")
-    parser.add_argument("--threads",         "-t", default=4,                      help="Number of threads to use")
-    parser.add_argument("--dataset_pattern", "-d", default = ".*",                 help="Regexp defining sampleSets to check")
+    parser.add_argument("--nevents_file",    "-e", default="",                             help="file containing number of events that will be put into output file")
+    parser.add_argument("--xsection_file",   "-x", default="",                             help="config file with cross sections that will be put into output file")
+    parser.add_argument("--input_file",      "-i", default="../sampleSets.cfg",            help="Existing SampleSets.cfg file")
+    parser.add_argument("--output_file",     "-o", default="../sampleSets_v1.cfg",         help="New SampleSets.cfg file to create")
+    parser.add_argument("--threads",         "-t", default=4,                              help="Number of threads to use")
+    parser.add_argument("--dataset_pattern", "-d", default = ".*",                         help="Regexp defining sampleSets to check")
     parser.add_argument("--legacy",          "-l", default = False, action = "store_true", help="Use legacy ntuples instead of Nano AOD ntuples")
     
     options          = parser.parse_args()
-    input_file       = options.input_file
     nevents_file     = options.nevents_file
+    xsection_file    = options.xsection_file
+    input_file       = options.input_file
     output_file      = options.output_file
     threads          = int(options.threads)
     dataset_pattern  = options.dataset_pattern
     legacy           = options.legacy
 
-    print "  samples file: {0}".format(input_file)
+    print "Settings:"
     print "  nevents file: {0}".format(nevents_file)
+    print "  xsection file: {0}".format(xsection_file)
+    print "  input file: {0}".format(input_file)
     print "  output file: {0}".format(output_file)
     print "  use legacy ntuples: {0}".format(legacy)
 
     try:
         inputSamples  = open(input_file, 'r')
-    except:
+    except IOError:
         print TextColor.red + "ERROR opening input file {0}".format(input_file) + TextColor.end
         return
     try:
         outputSamples  = open(output_file, 'w')
-    except:
+    except IOError:
         print TextColor.red + "ERROR opening output file {0}".format(output_file) + TextColor.end
         return
     
     ss = SampleSet(input_file)
     samples = [(name, s_file.replace("/eos/uscms", "root://cmseos.fnal.gov/")) for name, s_file in ss.sampleSetList()]
     
+    xsection_dict = {}
     weight_dict = {}
-    
+
+    # initialize files to false
     neventsFile = False
+    xsectionFile = False
+
+    # get nevents from file
     if nevents_file:
         # open nevents file if it is provided
         try:
             neventsFile = open(nevents_file, 'r')
-        except:
+        except IOError:
             print TextColor.red + "ERROR opening nevents file {0}".format(nevents_file) + TextColor.end
             return
+    
+    # get xsections from file
+    elif xsection_file:
+        # open xsection file if it is provided
+        try:
+            xsectionFile = open(xsection_file, 'r')
+            for line in xsectionFile:
+                # be careful: strip() removes spaces and endlines
+                line = line.strip()
+                if line:
+                    #print line
+                    sample_list = list(x.strip() for x in line.split(','))
+                    if len(sample_list) > 5:
+                        name = sample_list[0] 
+                        if name[0] == "#":
+                            name = name[1:].strip()
+                        xsection_dict[name] = sample_list[4]
+        except IOError:
+            print TextColor.red + "ERROR opening xsection file {0}".format(xsection_file) + TextColor.end
+            return
+
+    # calculate nevents
     else:
         # only get weights using getNEvts() if nevents_file is not provided by user
         for name, s_file in samples:
@@ -179,12 +231,19 @@ def main():
         if not sample:
             outputSamples.write(newSample)
             continue
+        # if xsection_file is provided, replace cross sections
+        if xsection_file:
+            newSample = updateXSection(sample, xsection_dict)
+            outputSamples.write(newSample)
+            continue
+
         # if first non-space character is # (comment), write original line (with endline) to file
         if sample[0] == "#":
             outputSamples.write(newSample)
             continue
         # if sample matches pattern, assume that it is a sample and get new sample to write to file
         if re.search(dataset_pattern, sample):
+            print "getNewSample() TIME"
             newSample = getNewSample(sample, weight_dict, neventsFile)
             #print "newSample: {0}".format(newSample)
         outputSamples.write(newSample)
@@ -193,6 +252,8 @@ def main():
     outputSamples.close()
     if neventsFile: 
         neventsFile.close()
+    if xsectionFile: 
+        xsectionFile.close()
     print TextColor.green + "Config file updates are finished!" + TextColor.end
 
 
